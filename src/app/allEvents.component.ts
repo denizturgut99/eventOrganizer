5546406e003d328e148ef36c2c7c137aeb834b69
denizturgut99 <denizturgut1999@hotmail.com>
2019-06-10 01:16:14 +0200

    added cities and times

--- src/app/allEvents.component.ts
+++ src/app/allEvents.component.ts
@@ -5,21 +5,18 @@ import { Component } from '@angular/core'
     selector: 'events',//<events>
     template: `<h2>{{ title }}</h2>
                 <div>
-                    <div *ngFor='let event of events'>
-                        {{ event }}
+                    <div class='events container' *ngFor='let event of events'>
+                    <div class='time'> {{ event.eventTime }} </div>
+                        <div class='allEvents'>
+                            <p> {{ event.hourTime }} {{ event.name }} </p>
+                            <p class='city'> {{ event.cityName }} </p>
+                        </div>
                     </div>
                 </div>`
 })
 export class EventsComponent {
     title = "List of Events";
-    allEvents;
     events;
-
-    // constructor(service: EventsService) {
-    //     this.allEvents = service.getEvents();
-    //     this.events = service.cityData;
-    //     this.getAllEvts();
-    // }
 
     constructor() {
         this.getEvents()
@@ -32,17 +29,62 @@ export class EventsComponent {
             })
             .then(json => {
                 let evt = json;
-                let test = [];
                 let names = [];
-                console.log(evt.length)
+                let dates = [];
+                
+                // console.log(evt.length)
                 for(let i = 0; i < evt.length; i++) {
-                    test.push(Object(evt[i]))
-                    // console.log(test)
-                    names.push(Array(evt[i].name))
-                    // console.log(names)
+                    names.push(Object(evt[i]))
+                    // dates.push(Array(evt[i].startDate))
                 }
-                this.events = names
+                this.events = names;
+                this.getCities();
+                this.getHours();
             })
             .catch(error => console.log(error));
+    }
+
+    getHours() {
+        let eventData = this.events;
+        let times = [];
+
+        for(let i = 0; i < eventData.length; i++) {
+            times.push(eventData[i].startDate)
+            for(let y = 0; y < times.length; y++) {
+                eventData[i]['eventTime'] = times[y].split('T')[1].slice(0, 5)
+            }
+        }
+        this.events = eventData
+        console.log(this.events)
+    }
+
+    getCities() {
+        let ct;
+        let allEvts = this.events
+        
+        fetch("http://localhost:3000/cities")
+        .then(response => {
+            return response.json()
+        })
+        .then(json => {
+            ct = json
+            assignCityNames();
+        })
+        .catch(error => console.log(error));
+
+        function assignCityNames() {
+            let evtData = allEvts;
+            
+            for(let i = 0; i < evtData.length; i++) {
+                for(let y = 0; y < ct.length; y++) {
+                    if(evtData[i].city == ct[y].id) {
+                        evtData[i]['cityName'] = ct[y].name
+                    }
+                }
+            }
+            allEvts = evtData;
+        }
+        this.events = allEvts;
+        // console.log(this.events)
     }
 }
